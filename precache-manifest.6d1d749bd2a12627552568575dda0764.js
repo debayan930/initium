e225bb5d6c848d550d2a380882e7d02c3bc6d91e
Debayan Das <debayan930@gmail.com>
2020-11-30 11:19:50 +0530

    Updates

---
+++ precache-manifest.6d1d749bd2a12627552568575dda0764.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e558ed2f294ef8a71b6a4ed86044246a",
+    "url": "/debayan930/initium/index.html"
+  },
+  {
+    "revision": "6c81e602702630335446",
+    "url": "/debayan930/initium/static/css/2.346cbe0d.chunk.css"
+  },
+  {
+    "revision": "94de20f14e88c7a399aa",
+    "url": "/debayan930/initium/static/css/3.34d8af1c.chunk.css"
+  },
+  {
+    "revision": "c24d86a5b4293eb0106d",
+    "url": "/debayan930/initium/static/css/main.4b83a682.chunk.css"
+  },
+  {
+    "revision": "6c81e602702630335446",
+    "url": "/debayan930/initium/static/js/2.3c8e7b15.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/debayan930/initium/static/js/2.3c8e7b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94de20f14e88c7a399aa",
+    "url": "/debayan930/initium/static/js/3.1992f850.chunk.js"
+  },
+  {
+    "revision": "c24d86a5b4293eb0106d",
+    "url": "/debayan930/initium/static/js/main.3b8f7654.chunk.js"
+  },
+  {
+    "revision": "28c70b857909b26abaa5",
+    "url": "/debayan930/initium/static/js/runtime-main.c8b8c67e.js"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/debayan930/initium/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/debayan930/initium/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/debayan930/initium/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/debayan930/initium/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/debayan930/initium/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/debayan930/initium/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/debayan930/initium/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/debayan930/initium/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/debayan930/initium/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/debayan930/initium/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/debayan930/initium/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/debayan930/initium/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/debayan930/initium/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/debayan930/initium/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/debayan930/initium/static/media/fa-solid-900.8e1ed89b.woff2"
+  }
+]);
